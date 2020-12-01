28eddb2f15239a68df34ff95c5000a4de832abf4
tetlie <marius.tetlie@gmail.com>
2020-12-01 22:52:19 +0100

    restructuring components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import ToDoItem from './components/ToDoItem/ToDoItem'
+import Header from './components/Header/Header'
+import ToDoList from './components/ToDoList/ToDoList'
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      content: 'Pickup dry cleaning',
-      isCompleted: true,
-    },
-    {
-      content: 'Get haircut',
-      isCompleted: false,
-    },
-    {
-      content: 'Build a todo app in React',
-      isCompleted: false,
-    }
-  ]);
 
-  
   return (
     <div className="app">
-      <div className="header">
-        {/* <img src={logo} className="logo" alt="logo" /> */}
-        <h1>Din to-do liste</h1>
-        <p>Trykk enter for å legge til et nytt element</p>
-        <p>Fjern innholdet i elementet for å slette det</p>
-      </div>
-      <form className="todo-list">
-        <ul>
-        <ToDoItem
-          toDo={todos}
-          func={setTodos}
-        />
-        </ul>
-      </form>
+      <Header />
+      <ToDoList />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './Header.css';
+
+function Header() {
+  return (
+      <div className="header">
+        {/* <img src={logo} className="logo" alt="logo" /> */}
+        <h1>What will you do today?</h1>
+      </div>
+  );
+}
+
+export default Header;
